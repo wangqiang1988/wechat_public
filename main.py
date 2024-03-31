cdd3a7b896a9e85c013d0292adc5f27a42e7b959
wangqiang1988 <298102526@qq.com>
2024-03-31 23:51:19 +0800

    update

--- main.py
+++ main.py
@@ -74,8 +74,6 @@ def upload_imagelist():
     RESULT_p = ''
     for img in media_url_p:
         RESULT_p += "<img src='%s'"%str(img)+ "/>" +'<br><br>' 
-    print(THUMB_MEDIA_ID_p)
-    print(RESULT_p)
     return THUMB_MEDIA_ID_p,RESULT_p
 
 
@@ -133,7 +131,6 @@ def main():
         thum_id, media_url = upload_imagelist()
     except:
         thum_id, media_url = upload_imagelist()
-    print(media_url)
     news_id = upload_wechat_news( current_time + '-'+ options.wechat_title,thum_id,options.wechat_disgest,media_url,"image_from_bing",token)
     if options.publish == 'yes':
         publish(token,news_id['media_id']) #发布
