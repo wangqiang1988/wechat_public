63b868877a42b98396fd3771dc099d8ca2dfd41f
wangqiang1988 <298102526@qq.com>
2024-03-31 20:26:11 +0800

    update

--- plog.py
+++ plog.py
@@ -5,11 +5,12 @@ from BingImageCreator import ImageGen
 
 
 bing_cookie = os.environ.get('bing_cookie')
+bing_user = os.environ.get('bing_user')
 def make_pic_and_save(sentence):
     try:
         print(bing_cookie)
         # for bing image when dall-e3 open drop this function
-        i = ImageGen(bing_cookie,bing_cookie)
+        i = ImageGen(bing_cookie,bing_user)
         images = i.get_images(sentence)
         new_path = os.path.join("./", 'output')
         if not os.path.exists(new_path):
