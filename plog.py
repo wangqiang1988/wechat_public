4bb331912f3ab848f27c75dde381bfe06ae98896
wang.wangqiang <wang.wangqiang@zhaopin.com.cn>
2024-04-02 11:05:53 +0800

    update

--- plog.py
+++ plog.py
@@ -7,7 +7,6 @@ from BingImageCreator import ImageGen
 def make_pic_and_save(bing_cookie,sentence):
     try:
         i = ImageGen(bing_cookie)
-        print(bing_cookie)
         images = i.get_images(sentence)
         new_path = os.path.join("/home/ubuntu/github/wechat_public", 'output')
         if not os.path.exists(new_path):
